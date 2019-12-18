aae4e1d0c113288a2e72de227dca537806dfbd7d
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-18 22:22:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil 
   
   name_hash.each do |key, value|
-    if min > value 
+    if min > value || value = 0 
       min = value
       min_key = key 
     end 
